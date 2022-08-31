4e736b8d7fec0874beaf8e77bf8fe8b82a38d87f
Your Name <you@example.com>
2022-08-31 22:23:47 +0800

    Default Changelist

---
+++ pu1/src/gfs.java
@@ -0,0 +1,5 @@
+public class gfs {
+    public static void main(String[] args){
+        System.out.println("afagsfas");
+    }
+}
---
+++ pu1/src/hfa.java
@@ -0,0 +1,5 @@
+public class hfa {
+    public static void main(String[] args) {
+        System.out.printf("阿斯顿");
+    }
+}
