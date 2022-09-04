572bea1ba60e7581881b9ab54bbad0f0360af161
Your Name <you@example.com>
2022-09-04 15:40:44 +0800

    Default Changelist

--- pu1/src/gfs.java
+++ pu1/src/gfs.java
@@ -1,5 +1,7 @@
 public class gfs {
     public static void main(String[] args){
-        System.out.println("afagsfas");
+        System.out.print("afagsfas");
+        System.out.println("dsgku");
+        System.out.print("dsgag");
     }
 }
