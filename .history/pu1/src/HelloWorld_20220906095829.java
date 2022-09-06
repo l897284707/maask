61c6d59f39675f1bbb3a8d83991551f26a551917
Your Name <you@example.com>
2022-09-06 10:59:07 +0800

    修改

--- .history/pu1/src/HelloWorld_20220906095829.java
+++ .history/pu1/src/HelloWorld_20220906095829.java
@@ -2,6 +2,6 @@ import com.sun.xml.internal.ws.wsdl.WSDLOperationFinder;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.printf("热太阳能");
+        System.out.printf("");
     }
 }
