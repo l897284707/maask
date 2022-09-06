54324bffc0342785afb315d7a7e8ca5a70982fb3
Your Name <you@example.com>
2022-09-06 20:16:07 +0800

    更新

---
+++ pu1/src/CharDemo.java
@@ -0,0 +1,8 @@
+public class CharDemo {
+    public static void main(String[] args){
+        System.out.print("转义字符测试\n");
+        System.out.print("我想输出一个回车符");
+        System.out.print("转义字符\r");
+        System.out.print("我想输出一个回符");
+    }
+}
--- pu1/src/HelloWorld.java
+++ pu1/src/HelloWorld.java
@@ -2,6 +2,22 @@ import com.sun.xml.internal.ws.wsdl.WSDLOperationFinder;
 
 public class HelloWorld {
     public static void main(String[] args) {
+        byte b=10;
+        short s = b;
+        int i = s;
+        long lon = i;
+        //  char --> int
+        char sex='男';
+        int sex2=sex;
+        System.out.println(sex2);
+//          float 和 double属于近似值
+        float f = 20f;
+        double d = f;
+        System.out.println(f);
+
+        int num = 2147483000;
+        float num2 = num;
+        System.out.println(num2);
         System.out.printf("热太阳能");
     }
 }
